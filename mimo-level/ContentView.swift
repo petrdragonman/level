386b1b852b19b0c1a907404a43ca9fd193a74d55
petrdragonman <petrdragonman@me.com>
2026-04-23 18:22:28 +1000

    Initial Commit

---
+++ mimo-level/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mimo-level
+//
+//  Created by Petr Valouch on 23/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mimo-level/mimo_levelApp.swift
@@ -0,0 +1,17 @@
+//
+//  mimo_levelApp.swift
+//  mimo-level
+//
+//  Created by Petr Valouch on 23/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct mimo_levelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
